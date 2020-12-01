f711f7b6cba7441e7acf32d62dff711daece4536
MatthisBrault <matthis.brault@my-digital-school.org>
2020-12-01 14:26:00 +0100

    ajout des produits dans la page d'acceuil

--- var/cache/prod/smarty/cache/ps_customtext/1/1/8/classic/ca/f3/b9/caf3b9a61e7d5916f8d401ed4b0c56fde089d766.pscustomtextpscustomtext..php
+++ var/cache/prod/smarty/cache/ps_customtext/1/1/8/classic/ca/f3/b9/caf3b9a61e7d5916f8d401ed4b0c56fde089d766.pscustomtextpscustomtext..php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-11-30 12:44:41
+/* Smarty version 3.1.34-dev-7, created on 2020-12-01 14:22:17
   from 'module:pscustomtextpscustomtext.' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5fc4db2940b059_12280494',
+  'unifunc' => 'content_5fc64389065777_93579517',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -18,12 +18,11 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-function content_5fc4db2940b059_12280494 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fc64389065777_93579517 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div id="custom-text">
-  <h2>Custom Text Block</h2>
-<p><strong class="dark">Lorem ipsum dolor sit amet conse ctetu</strong></p>
-<p>Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+  <h2>Bijoure </h2>
+<p>bienvenue au marché de noël de triffouilli les framboise dans le 36.</p>
 </div>
 <?php }
 }
